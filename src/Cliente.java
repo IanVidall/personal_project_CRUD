eec5b8f702dea5b2ab1ac6335df6459d42cf40fd
Ian Vidal <ianvidal@live.com>
2023-03-03 23:10:34 -0300

    create a method register

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,19 +1,32 @@
+import java.util.ArrayList;
 
 public class Cliente {
 	private int id;
 	private String nome;
 	private String cpf;
+	private static int proximoId = 1;
 
-	
+	ArrayList<Cliente> listClientes = new ArrayList<Cliente>();
+
 	public Cliente() {
 
 	}
 
-	public Cliente(int id, String nome, String cpf) {
+	public void cadastrarCliente(String nome, String cpf) {
+		Cliente novoCliente = new Cliente(nome, cpf);
+		listClientes.add(novoCliente);
+	}
+
+	public void excluirCliente() {
+		listClientes.remove(id);
+	}
+
+	public Cliente(String nome, String cpf) {
 		super();
-		this.id = id;
 		this.nome = nome;
 		this.cpf = cpf;
+		this.id = proximoId;
+		proximoId++;
 	}
 
 	public String getNome() {
@@ -38,6 +51,22 @@ public class Cliente {
 
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
+	}
+
+	public void menuText() {
+		System.out.println("************************************************");
+		System.out.println("***           Registro de Cliente            ***");
+		System.out.println("************************************************");
+		System.out.println(" 1 - Cadastrar Cliente");
+		System.out.println(" 2 - Excluir Cliente");
+		System.out.println(" 3 - Ir Para Filmes");
+		System.out.println(" 4 - Mostrar Todos os Clientes");
+		System.out.println(" 5 - Sair.");
+
+	}
+
+	public Cliente toString() {
+		return listClientes.get(id)  ;
 	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,42 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main  extends Cliente{
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
 		Cliente cliente = new Cliente();
-		Cliente cliente1 = new Cliente();
-		Cliente cliente2 = new Cliente();
-		
-		
-		
-		System.out.println("************************************************");
-		System.out.println("***           Registro de Cliente            ***");
-		System.out.println("************************************************");
-		System.out.println(" 1 - Cadastrar Cliente");
-		System.out.println(" 2 - Excluir Cliente");
-		System.out.println(" 3 - Atualizar");
-		System.out.println(" 4 - Mostrar Tudo.");
-		System.out.println(" 5 - Sair.");
-		int opc = sc.nextInt();
-		switch (opc) {
-		case 1 :
-			
-			System.out.println("Cadastrar Cliente");
-			System.out.print("Nome : ");
-			
-			sc.nextLine();
-			String name = sc.nextLine();
-			cliente.setNome(name);
-			
-			System.out.println("Cpf : ");
-			String cpf = sc.nextLine();
-			cliente.setCpf(cpf);
-		}
-			
-		
-		
-		
-		sc.close();
-	}
 
+		
+		do {
+			cliente.menuText();
+			
+			int opc = sc.nextInt();
+			switch (opc) {
+			case 1:
+				
+				sc.nextLine();
+				System.out.print("Nome : ");
+				String name = sc.nextLine();
+				System.out.print("Cpf : ");
+				String cpf = sc.nextLine();
+				cliente.cadastrarCliente(name, cpf);
+				System.out.println("TOTAL DE CLIENTES: "+cliente.listClientes.size());
+				break;
+				
+			case 2:
+				System.out.println("Qual Cliente deseja Excluir ? : ");
+				
+				
+			case 4:
+				
+				System.out.println(cliente.listClientes.toString());
+				
+				
+			}
+				
+			
+			
+		} while (true);
+	}
 }
